fd614938267eb94ccdf4e5c268839cab80901cdc
Tinny <mos23018@byui.edu>
2025-01-17 10:20:35 +0200

    files added

---
+++ products/product-data.js
@@ -0,0 +1,16 @@
+export const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    description: "This is a detailed description of Product 1.",
+    price: "$10.00",
+    image: "path/to/image1.jpg",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    description: "This is a detailed description of Product 2.",
+    price: "$15.00",
+    image: "path/to/image2.jpg",
+  }
+];
